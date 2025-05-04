e374f1d15dbc6106761cbbfe8412a77c7ed3b0d8
saurabhojha <ojhasaurabh2099@gmail.com>
2025-05-05 01:49:23 +0530

    Remove initial

--- open-table-formats/iceberg/poc/clickhouse/initial/teardown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🧹 Stopping and cleaning up containers, and local folders..."
-docker-compose down -v
-docker rm $(docker ps -a -q)
-rm -rf ./lakehouse ./notebooks ./minio ./clickhouse
-echo "✅ Cleanup complete!"
